dcbd3150de069cb457a4dfa531614f8beea0d7e1
ivandacruz <64236894+ivandacruz@users.noreply.github.com>
2021-10-18 16:53:35 -0300

    Ajustes em codigo

--- app/src/main/java/com/ivan/coin_converter/core/exceptions/RemoteException.kt
+++
@@ -1,3 +0,0 @@
-package com.ivan.coin_converter.core.exceptions
-
-class RemoteException(override val message: String) : Throwable()
--- app/src/main/java/com/ivan/coin_converter/data/model/ErrorResponse.kt
+++
@@ -1,7 +0,0 @@
-package com.ivan.coin_converter.data.model
-
-data class ErrorResponse (
-    val status: Long,
-    val code: String,
-    val message: String
-)
